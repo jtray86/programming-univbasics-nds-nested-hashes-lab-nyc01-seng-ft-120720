a2e7a94dbe2c5e96e6f3449f89925dbb2c762552
Jen Tracy <jenniferetracy@gmail.com>
2020-11-16 16:42:32 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -31,19 +31,8 @@ def literal_aoh
 end
 
 def aoh_lookup(aoh, row, key)
-   new_aoh=[
-    {:name => "Don Gately", :occupation => "Live-in Staff"},
-    {:name => "Joelle van Dyne",
-    :occupation =>"Radio Personality" },
-    { :name =>  "Pat Monteseian",
-     :occupation => "Staff" },
-    {:name => "Kate Gompert",
-    :occupation =>"None" },
-    {:name => "Bruce Green",
-    :occupation =>"Fan of Mildred" }
-    ]
-    
-    new_aoh[0]{3}[:name]
+   aoh[row][key]
+   
   
 end
 
